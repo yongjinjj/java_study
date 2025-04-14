6daf87e32aaa8a98929a8686f4d7e2758f3e1593
yongjinjj <yjnr10@gmail.com>
2025-04-14 11:35:31 +0900

    자바 시작, gitignore 추가

---
+++ src/study/start/Hello.java
@@ -0,0 +1,20 @@
+package study.start;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!!");
+		
+		// 실행(Run) : Ctrl + F11
+
+		// 주석
+//		단축키 : Ctrl + /
+//			   Ctrl + Shift + c
+		/*
+		 * 여러줄 주석
+		 *    
+		 */
+	}
+}
