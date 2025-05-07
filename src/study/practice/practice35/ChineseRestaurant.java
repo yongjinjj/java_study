e8d9139f53b463ee5c22879aff35e12e8ddb38bd
yongjinjj <yjnr10@gmail.com>
2025-05-07 16:00:12 +0900

    연습35+ ArrayList 변환

--- src/study/practice/practice35/ChineseRestaurant.java
+++ src/study/practice/practice35/ChineseRestaurant.java
@@ -1,5 +1,7 @@
 package study.practice.practice35;
 
+import java.util.ArrayList;
+
 public class ChineseRestaurant {
 	public static void main(String[] args) {
 		// 주문을 위한 메뉴 선정
@@ -8,14 +10,32 @@ public class ChineseRestaurant {
 		Menu tangsook = new Menu("탕수육", 13900);
 		// 메뉴를 조합하여 주문을 생성
 		Menu[] menuArr = { jjajang, jjambbong, tangsook };
-		Order order = new Order(123, menuArr);
+		
+		ArrayList<Menu> menuList = new ArrayList<Menu>();
+		menuList.add(jjajang);
+		menuList.add(jjambbong);
+		menuList.add(tangsook);
+		
+		//Order order = new Order(123, menuArr);
+		Order order = new Order(123, menuList);
+		
 		// 주문 결과 출력
 		System.out.printf("주문 합계: %d원\n", order.totalPrice());
 		
 		
-		Menu[] menuArr2 = {new Menu("짬뽕", 5900), new Menu("짜장", 4900)
-						, new Menu("탕수육", 13900) , new Menu("탕수육", 13900)};
-		Order order2 = new Order(124, menuArr2);
-		System.out.printf("[주문번호 %d] 주문 합계: %d원\n", order2.orderNum ,order2.totalPrice());
+		
+		
+//		Menu[] menuArr2 = {new Menu("짬뽕", 5900), new Menu("짜장", 4900)
+//						, new Menu("탕수육", 13900) , new Menu("탕수육", 13900)};
+//		Order order2 = new Order(124, menuArr2);
+//		System.out.printf("[주문번호 %d] 주문 합계: %d원\n", order2.orderNum ,order2.totalPrice());
+		ArrayList<Menu> menuList2 = new ArrayList<Menu>();
+		menuList2.add(new Menu("짬뽕", 5900));
+		menuList2.add(new Menu("짜장", 4900));
+		menuList2.add(new Menu("탕수육", 13900));
+		menuList2.add(new Menu("탕수육", 13900));
+		
+		Order order2 = new Order(124, menuList2);
+		System.out.printf("[주문번호 %d] 주문 합계: %d원\n", order2.orderNum , order2.totalPrice());
 	}
 }
--- src/study/practice/practice35/Order.java
+++ src/study/practice/practice35/Order.java
@@ -1,14 +1,24 @@
 package study.practice.practice35;
 
+import java.util.ArrayList;
+
 public class Order {
 	// 필드(인스턴스 필드)
 	int orderNum;
-	Menu[] menus;
+	//Menu[] menus;
+	ArrayList<Menu> menuList;
+	
 	// 생성자
+	/*
 	public Order(int i, Menu[] arr) {
 		orderNum = i;
 		menus = arr;
+	}*/
+	public Order(int i, ArrayList<Menu> menuList) {
+		orderNum = i;
+		this.menuList = menuList;
 	}
+	
 	// 메소드(인스턴스 메소드)
 	public int totalPrice() {
 		/* 1. 모든 주문 메뉴의 총합을 반환하세요. */
@@ -16,9 +26,16 @@ public class Order {
 		//주문한 메뉴는 menus 
 		//메뉴당 금액 menus -> Menu -> price
 		int sum = 0;
-		for(int i=0; i<menus.length; i++) {
-			sum += menus[i].price;
+		
+		for(int i=0; i<menuList.size(); i++) {
+			sum += menuList.get(i).price;
+			//	   menuList.get(i) : i인덱스 위치에 있는 Menu 객체 .price
+			//	   menus[i]		   : i인덱스 위치에 있는 Menu 객체 .price
 		}
+		
+//		for(int i=0; i<menus.length; i++) {
+//			sum += menus[i].price;
+//		}
 		
 //		for(Menu menu : menus) {
 //			sum += menu.price;
