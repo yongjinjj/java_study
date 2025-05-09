245ed4a01d469ac680157d22c67ff9163b86989b
yongjinjj <yjnr10@gmail.com>
2025-05-09 14:30:40 +0900

    클래스 상속 강제타입변환

--- src/study/inherit/inherit06/Bus.java
+++ src/study/inherit/inherit06/Bus.java
@@ -4,4 +4,8 @@ public class Bus extends Vehicle {
 	public void run() {
 		System.out.println("Bus run");
 	}
+	
+	public void start() {
+		System.out.println("운행 시작~");
+	}
 }
--- src/study/inherit/inherit06/Main.java
+++ src/study/inherit/inherit06/Main.java
@@ -27,6 +27,43 @@ public class Main {
 		//Bus b4 = takeVehicle();
 		//Bus = Vehicle
 		Vehicle v4 = takeVehicle();
+		
+		//------------------------------
+		
+		Vehicle vv = new Vehicle();
+		
+		Vehicle vv2 = new Bus();
+		//vv2.run();		O
+		//vv2.start();		X
+		Bus bb = new Bus();
+		bb.run();
+		bb.start();
+		
+		Bus bb2 = (Bus)vv2;
+		bb2.start();
+		
+//		Bus bb3 = (Bus)vv;
+//		bb3.start();
+		
+		
+		//---------------------
+		Vehicle vv4 = takeVehicle();
+		
+		
+		if( vv4 instanceof Bus ) {
+			Bus bbb = (Bus)vv4;
+			System.out.println("Bus가 들어있음");
+		}
+		
+		if( vv4 instanceof Taxi ) {
+			Taxi ttt = (Taxi)vv4;
+			System.out.println("Taxi가 들어있음");
+			
+		}
+		
+		//Bus bbb = (Bus)vv4;
+		//bbb.start();
+		
 	}
 	
 	
@@ -45,8 +82,16 @@ public class Main {
 		//Vehicle v = new Taxi();
 		
 //		return new Vehicle();
-		return new Bus();
+//		return new Bus();
 //		return new Taxi();
+		
+		int n = (int)(Math.random()*2) + 1;
+		
+		if(n == 1)
+			return new Bus();
+		else
+			return new Taxi();
+				
 		
 	}
 
